fbab87bbe50d38923b6c31b08411cffb83181353
JiiXaa <anythingfx@gmail.com>
2025-03-14 12:22:22 +0000

    fix(neo-tree): update config for latest version, migrate follow_current_file, and fix refresh command

--- lua/jiixaa/plugins/nvim-neo-tree.lua
+++ lua/jiixaa/plugins/nvim-neo-tree.lua
@@ -7,19 +7,26 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
-    require("neo-tree").setup({
+    local neotree = require("neo-tree")
+
+    neotree.setup({
       window = {
-        width = 30,
+        position = "left",
+        width = 35,
       },
+
       filesystem = {
+        follow_current_file = {
+          enabled = true,
+        },
+        use_libuv_file_watcher = true,
         filtered_items = {
           hide_dotfiles = false,
           hide_gitignored = false,
           hide_by_name = { ".DS_Store" },
         },
-        follow_current_file = true,
-        use_libuv_file_watcher = true,
       },
+
       default_component_configs = {
         indent = { with_markers = true },
         icon = {
@@ -28,5 +35,18 @@ return {
         },
       },
     })
+
+    vim.keymap.set("n", "<leader>ee", "<cmd>Neotree toggle<CR>", { desc = "Toggle Neo-tree" })
+    vim.keymap.set("n", "<leader>ef", "<cmd>Neotree reveal<CR>", { desc = "Reveal current file in Neo-tree" })
+    vim.keymap.set("n", "<leader>eb", "<cmd>Neotree buffers toggle<CR>", { desc = "Toggle Buffers in Neo-tree" })
+    vim.keymap.set("n", "<leader>eg", "<cmd>Neotree git_status toggle<CR>", { desc = "Toggle Git Status in Neo-tree" })
+    vim.keymap.set(
+      "n",
+      "<leader>er",
+      "<cmd>Neotree close<CR> | <cmd>Neotree reveal<CR>",
+      { desc = "Soft Refresh Neo-tree" }
+    )
+    vim.keymap.set("n", "<leader>ew", "<cmd>Neotree focus<CR>", { desc = "Focus on Neo-tree" })
+    vim.keymap.set("n", "<leader>eC", "<cmd>Neotree close<CR>", { desc = "Close Neo-tree" })
   end,
 }
