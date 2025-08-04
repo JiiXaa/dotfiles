2822ea791b727f23530b4a6c5560349b9f126bd3
JiiXaa <anythingfx@gmail.com>
2025-08-04 22:53:22 +0100

    chore(which-key): update config with centered layout and rounded border

--- .config/nvim/lua/jiixaa/plugins/which-key.lua
+++ .config/nvim/lua/jiixaa/plugins/which-key.lua
@@ -2,12 +2,27 @@ return {
   "folke/which-key.nvim",
   event = "VeryLazy",
   init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 500
+    vim.o.timeout = true -- Enable timeout for mapped sequences
+    vim.o.timeoutlen = 500 -- Timeout delay for key sequences (ms)
   end,
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+    plugins = {
+      spelling = true, -- Show spelling suggestions
+      presets = {
+        operators = false, -- Don't show mappings for operators like d, c, y
+        motions = true, -- Show motion mappings (e.g., ], [)
+        text_objects = true, -- Show text object mappings (e.g., a", i()
+        windows = true, -- Show window commands (<C-w>)
+        nav = true, -- Show navigation mappings (e.g., ]q, [a)
+        z = true, -- Show mappings under 'z'
+        g = true, -- Show mappings under 'g'
+      },
+    },
+    win = {
+      border = "rounded", -- Rounded border for the popup window
+    },
+    layout = {
+      align = "center", -- Center align the popup content
+    },
   },
 }
